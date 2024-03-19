a79acde89a0bb2787aac85c0390a67bf3558a0d2
Carlos Mendoza <c.mendozar@outlook.com>
2024-03-18 21:43:18 -0300

    First Commit: Set up the first enviroment and repository structure

---
+++ set-up.sh
@@ -0,0 +1,9 @@
+# I consider you already have installed python3 in your machine
+
+# Install virtual enviroments
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+
+# Make repository structure
+mkdir data
