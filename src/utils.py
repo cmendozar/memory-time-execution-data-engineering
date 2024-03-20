2b69cd0cc0004ff4d83684a42d2d0781e6faae7f
Carlos Mendoza <c.mendozar@outlook.com>
2024-03-20 00:03:03 -0300

    feat: add utils folder and timer method

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function '{func.__name__}' executed in {end_time - start_time:.6f} seconds")
+        return result
+    return wrapper
+
+# python3 -m cProfile <python_file-py>
