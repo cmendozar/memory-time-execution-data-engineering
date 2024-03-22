9169ca403c554c3851fa549fb5475915ae52a1ba
Carlos Mendoza <c.mendozar@outlook.com>
2024-03-21 22:25:03 -0300

    feat: add q1 solutions for memory and time approachs

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,55 @@
+import json
+from collections import Counter, defaultdict
 from typing import List, Tuple
 from datetime import datetime
 
+from memory_profiler import profile
+from utils import timer
+
+
+def count_dates(file_path: str) -> dict:
+    date_user_counters = defaultdict(Counter)
+    with open(file_path, "r") as file:
+        for line in file:
+            try:
+                tweet = json.loads(line)
+                date = datetime.fromisoformat(tweet['date']).date()
+                user_name = tweet.get("user", {}).get("username", "unknown")
+                date_user_counters[date][user_name] += 1
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON in line: {line}. Error: {e}")
+            except KeyError as e:
+                print(f"Missing 'date' key in JSON: {line}. Error: {e}")
+    return date_user_counters
+
+
+def get_top_10_dates(date_user_counters: dict) -> List[Tuple[datetime.date, int]]:
+    all_dates = [(date, sum(user_counter.values())) for date, user_counter in date_user_counters.items()]
+    all_dates.sort(key=lambda x: x[1], reverse=True)
+    return all_dates[:10]
+
+
+def get_top_users_per_date(
+        date_user_counters: dict) -> List[Tuple[datetime.date, str]]:
+    result = []
+    top_10_dates = get_top_10_dates(date_user_counters)
+    for date, count in top_10_dates:
+        user_counter = date_user_counters[date]
+        top_user = user_counter.most_common(1)[0][0]\
+            if user_counter else "unknown"
+        result.append((date, top_user))
+    return result
+
+
+@timer
+@profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    dates_data = count_dates(file_path)
+    result = get_top_users_per_date(dates_data)
+    return result
+
+
+if __name__ == "__main__":
+    file_path = "data/farmers-protest-tweets-2021-2-4.json"
+    result = q1_memory(file_path)
+    print(result)
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,63 @@
+import pandas as pd
+import json
+
 from typing import List, Tuple
 from datetime import datetime
 
+from memory_profiler import profile
+from utils import timer
+
+
+def json_to_df(file_path: str) -> pd.DataFrame:
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            tweet_dict = {}
+            try:
+                tweet = json.loads(line)
+                tweet_dict['date'] = tweet['date']
+                tweet_dict['user_name'] = tweet.get(
+                    'user', {}
+                ).get('username', 'unknown')
+                data.append(tweet_dict)
+            except json.JSONDecodeError:
+                print(f"Error decoding JSON in line: {line}")
+    df = pd.DataFrame(data)
+    df['date'] = pd.to_datetime(df['date']).dt.date
+    return df
+
+
+def get_top_10_dates(dates: pd.DataFrame) -> list:
+    """
+    Input: Dataframe with date columns from json file, this df
+    need to be with whole register.
+    Return: List of top 10 order by more tweets counts desc.
+    """
+    top_10 = dates.value_counts().index[:10]
+    return list(top_10)
+
+
+def get_tweeter_user_per_day(date: str, data: pd.DataFrame) -> str:
+    date_rows = data[data['date'] == date]
+    grouped_df = date_rows.groupby('user_name').count()
+    user = grouped_df.sort_values('date', ascending=False).head(1).index[0]
+    return user
+
+
+@timer
+@profile
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    result = list()
+    df = json_to_df(file_path)
+    date_df = df['date']
+    top_10_dates = get_top_10_dates(date_df)
+    for date in top_10_dates:
+        user = get_tweeter_user_per_day(date, df)
+        result.append((date, user))
+    return result
+
+
+if __name__ == '__main__':
+    file_path = 'data/farmers-protest-tweets-2021-2-4.json'
+    result = q1_time(file_path)
+    print(result)
